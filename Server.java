42e683b23fcb3a8cbd1134651fe165b3b2b37007
cdm2012 <christopher.d.miller.1@gmail.com>
2014-03-23 02:07:24 -0400

    The Server.java file will have a Client.java file
    to connect to it, and waits for the client connection. Once connected it then waits for client to input some text.

--- Server.java
+++ Server.java
@@ -23,30 +23,25 @@ public class Server {
 		ServerSocket welcomeSocket = new ServerSocket(8888);
 		SERVERIP = getLocalIpAddress();
 
-		System.out.println("Connected and waiting for client input!\n");
+		System.out.println("Server started and waiting for client connection!\n");
 		System.out.println("Listening on IP: " + SERVERIP + "\n\n");
 
 		while (true) {
 			Socket connectionSocket = welcomeSocket.accept();
+			String ip = connectionSocket.getInetAddress().toString().substring(1);
+			System.out.println("Connected to "+ip+", and waiting for client input!\n");
 
 			BufferedReader inFromClient = new BufferedReader(
 					new InputStreamReader(connectionSocket.getInputStream()));
 
-			DataOutputStream outToClient = new DataOutputStream(
-					connectionSocket.getOutputStream());
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
 			clientSentence = inFromClient.readLine();
-
-			String ip = connectionSocket.getInetAddress().toString()
-					.substring(1);
-
-			System.out
-					.println("In from client (" + ip + "): " + clientSentence);
+			System.out.println("In from client (" + ip + "): " + clientSentence);
 
 			if (clientSentence != null) {
 				capitalizedSentence = clientSentence.toUpperCase() + '\n';
-				System.out.println("Out to client (" + ip + "): "
-						+ capitalizedSentence);
+				System.out.println("Out to client (" + ip + "): " + capitalizedSentence);
 				outToClient.writeBytes(capitalizedSentence + "\n");
 			}
 
